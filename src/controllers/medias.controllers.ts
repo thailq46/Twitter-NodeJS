dcd9569c8da3bbd9bc01f6e8cd54645932cfdc73
thailq <xinphatmutikill@gmail.com>
2024-03-09 09:18:41 +0700

    feat(upload-media): Handle image with sharp

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -1,12 +1,12 @@
 import { Request, Response, NextFunction } from 'express'
-import { handleUploadSingleImage } from '~/utils/file'
+import mediasService from '~/services/medias.services'
 
 export const uploadSingleImageController = async (req: Request, res: Response, next: NextFunction) => {
   /**
    * Cách fix 1 ESModule được sử dụng trong CommonJS (bản ta dùng là types@formidable v3 không phải v2 lên ko cần -> Nếu formidable là v3 và types@formidable là v2 thì cần sử dụng để không bị lỗi)
    * const formidable = (await import('formidable')).default
    */
-  const data = await handleUploadSingleImage(req)
+  const data = await mediasService.handleUploadSingleImage(req)
   return res.status(200).json({
     result: data
   })
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,12 +1,12 @@
 import { NextFunction, Request } from 'express'
-import formidable from 'formidable'
+import formidable, { File } from 'formidable'
 import fs from 'fs'
 import path from 'path'
+import { UPLOAD_TEMP_DIR } from '~/constants/dir'
 
 export const initFolder = () => {
-  const uploadFolderPath = path.resolve('uploads/images')
-  if (!fs.existsSync(uploadFolderPath)) {
-    fs.mkdirSync(uploadFolderPath, {
+  if (!fs.existsSync(UPLOAD_TEMP_DIR)) {
+    fs.mkdirSync(UPLOAD_TEMP_DIR, {
       recursive: true // mục đích là để tạo folder nested
     })
   }
@@ -14,7 +14,7 @@ export const initFolder = () => {
 
 export const handleUploadSingleImage = async (req: Request) => {
   const form = formidable({
-    uploadDir: path.resolve('uploads/images'),
+    uploadDir: UPLOAD_TEMP_DIR,
     maxFiles: 1,
     keepExtensions: true,
     maxFileSize: 800 * 1024, // 800KB,
@@ -26,7 +26,7 @@ export const handleUploadSingleImage = async (req: Request) => {
       return valid
     }
   })
-  return new Promise((resolve, reject) => {
+  return new Promise<File>((resolve, reject) => {
     form.parse(req, (err, fields, files) => {
       // console.log('files', files)
       // console.log('fields', fields)
@@ -37,7 +37,12 @@ export const handleUploadSingleImage = async (req: Request) => {
       if (!Boolean(files.image)) {
         return reject(new Error('File is empty') as any)
       }
-      resolve(files)
+      resolve((files.image as File[])[0])
     })
   })
+}
+
+export const getNameFromFullname = (fullname: string) => {
+  const ext = path.extname(fullname)
+  return path.basename(fullname, ext)
 }
