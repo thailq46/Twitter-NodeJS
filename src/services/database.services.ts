dce040225084af5a818045d4ae8c650bc974e70d
thailq <xinphatmutikill@gmail.com>
2024-03-15 21:54:15 +0700

    feat(tweet): Create hashtags

--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -5,6 +5,7 @@ import RefreshToken from '~/models/schemas/RefreshToken.schema'
 import Follower from '~/models/schemas/Follower.schema'
 import VideoStatus from '~/models/schemas/VideoStatus.schema'
 import Tweet from '~/models/schemas/Tweet.schema'
+import Hashtag from '~/models/schemas/Hashtag.schema'
 config()
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@twitter.hdyolge.mongodb.net/?retryWrites=true&w=majority&appName=Twitter`
@@ -72,6 +73,10 @@ class DatabaseService {
 
   get tweets(): Collection<Tweet> {
     return this.db.collection(process.env.DB_TWEETS_COLLECTION as string)
+  }
+
+  get hashtags(): Collection<Hashtag> {
+    return this.db.collection(process.env.DB_HASHTAGS_COLLECTION as string)
   }
 
   async closeConnection() {
--- src/services/tweets.services.ts
+++ src/services/tweets.services.ts
@@ -1,15 +1,30 @@
 import {TweetRequestBody} from '~/models/request/Tweet.requests'
 import databaseService from './database.services'
 import Tweet from '~/models/schemas/Tweet.schema'
-import {ObjectId} from 'mongodb'
+import {ObjectId, WithId} from 'mongodb'
+import Hashtag from '~/models/schemas/Hashtag.schema'
 
 class TweetsService {
+  async checkandCreateHashtags(hashtags: string[]) {
+    const hashtagDocuments = await Promise.all(
+      hashtags.map((hashtag) => {
+        // Tìm hashtag trong db nếu có thì lấy ko thì tạo mới
+        return databaseService.hashtags.findOneAndUpdate(
+          {name: hashtag},
+          {$setOnInsert: new Hashtag({name: hashtag})},
+          {upsert: true, returnDocument: 'after'}
+        )
+      })
+    )
+    return hashtagDocuments.map((hashtag) => hashtag?._id)
+  }
   async createTweet(user_id: string, body: TweetRequestBody) {
+    const hashtags = await this.checkandCreateHashtags(body.hashtags)
     const result = await databaseService.tweets.insertOne(
       new Tweet({
         audience: body.audience,
         content: body.content,
-        hashtags: [], // Chỗ này chưa làm tạm thời để rỗng
+        hashtags: hashtags as ObjectId[],
         mentions: body.mentions,
         medias: body.medias,
         parent_id: body.parent_id,
