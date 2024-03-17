8590649eb9b9699a973e7a8b6e4f5149e4a284fb
thailq <xinphatmutikill@gmail.com>
2024-03-17 10:41:36 +0700

    feat(tweet): Fake data automatically with faker

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import staticRouter from './routes/static.routes'
 import cors from 'cors'
 import tweetsRouter from './routes/tweets.routes'
 import bookmarksRouter from './routes/bookmarks.routes'
+// import '~/utils/fake'
 
 config()
 databaseService.connect().then(() => {
